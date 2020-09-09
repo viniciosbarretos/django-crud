d0b90b9f76b7ebb2646c45b7a059cd997cf108b5
vinicios <vinicios.barretos@unesp.br>
2020-09-09 14:16:20 -0300

    wait postgres load to migrate database

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Wait postgres load
+RETRIES=15
+until psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  sleep 3
+done
+
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py makemigrations
