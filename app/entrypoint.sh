d69e6c3f347b77fa186c12520491577639c067e1
vinicios <vinicios.barretos@unesp.br>
2020-09-09 14:50:12 -0300

    fix wait time psql

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Wait postgres load
 RETRIES=15
-until psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+until python manage.py makemigrations > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
   sleep 3
 done
